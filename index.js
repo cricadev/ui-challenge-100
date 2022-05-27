e6e9494aea8fa9130c5f6559d44c4e75991b9a46
CristianN998 <cristiancasallas998@gmail.com>
2022-05-26 21:04:11 -0500

    fixed animations in home, signup form popping up

--- index.js
+++
@@ -1,9 +0,0 @@
-const signBtn = document.querySelector("#signForm");
-
-function clickHandler(e){
-  e.preventDefault();
-  console.log(e.target);
-  e.target.classList.add("active");
-}
-signBtn.addEventListener("click", clickHandler);
-console.log(signBtn);
