9ad5b4bbba5d3df66a35d7b1bd55e7dd062e2962
CristianN998 <cristiancasallas998@gmail.com>
2022-06-01 22:35:22 -0500

    login anims done, password strength checker with javascript done

---
+++ index.js
@@ -0,0 +1,48 @@
+const viewPass = document.querySelectorAll("#viewPass");
+const passInput = document.querySelectorAll("#password");
+const statsPass = document.querySelector("#stats");
+const securityText = document.querySelector("#securityText");
+
+let isVisible = true;
+viewPass.forEach(function(img){
+  img.addEventListener("click", function(e) {
+    e.target.classList.toggle("active")
+    e.target.classList.contains("active")
+    ? isVisible = false
+    : isVisible = true;
+  } 
+)});
+passInput.forEach(function(input){
+  input.addEventListener("mousemove", function(e) {
+    if(isVisible === false){
+      e.target.type = "text";
+    }
+    else{
+      e.target.type = "password";
+    }
+  })
+})
+passInput.forEach(function(input){
+  input.addEventListener("keydown", function(e) {
+    const regexAll = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const value = e.target.value;
+    const length = e.target.value.length 
+    if(length >= 0){
+      statsPass.classList.remove("highSec");
+      statsPass.classList.remove("medSec");
+      statsPass.classList.add("lowSec");
+      securityText.textContent = "low";
+    }
+    if(length >= 5 &&  /\d/.test(value) ){
+      statsPass.classList.remove("lowSec");
+      statsPass.classList.remove("highSec");
+      statsPass.classList.add("medSec");
+      securityText.textContent = "medium";
+    }
+    if(length >= 10 && regexAll.test(value) ) {
+      statsPass.classList.remove("lowSec");
+      statsPass.classList.remove("medSec");
+      statsPass.classList.add("highSec");
+      securityText.textContent = "high";
+    }
+  })});
